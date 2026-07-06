b83fbcf1af5be8edfdd51c36cd546eb2d1f15ad5
y栗子 <wangyi2010mabin@163.com>
2026-07-06 16:36:56 +0800

    1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LINKS = {
     //外出登记
     outing: "https://f.kdocs.cn/g/36UjdJ7f",
     //省公司登记
-    province: "http://221.207.18.198:18088/?company_id=233",
+    province: "https://visitor.zhhqznly.cn:18088/?company_id=233",
     //信通公司登记(海湖)
     xintong: "http://visitor.zhhqznly.cn:18088/?company_id=241"
 };
